18c48be02c872722d11e9a81d89e89413cc907be
ARTEM <artemsmirnof@mail.ru>
2024-08-21 20:43:41 +0300

    dcdc

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
+
+import { observer } from "mobx-react-lite";
+import langStore from "../store/langStore";
 import LangButt from "./LangButt";
 import styles from "./styles/header.module.scss"
 
 
-const MainPage = () => {
-
+const MainPage = observer(() => {
 
   
     return (
       <header className={styles.header}>
+        <div className={styles.title}>{langStore.dictionary[langStore.getlang()].about}</div>
         <LangButt/>
 
 
       </header>
     );
-  };
+  });
   
   export default MainPage;
   
