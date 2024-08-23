d455082ce72d2dbce04a54e74c22204333a9ecb2
ARTEM <artemsmirnof@mail.ru>
2024-08-23 18:16:13 +0300

    bggb

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,18 +1,21 @@
 
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import { useEffect } from 'react';
+import langStore from '../store/langStore';
+import { useInitData } from '@telegram-apps/sdk-react';
 
 const MainPage = () => {
-//     const initData = useInitData();
+    const initData = useInitData();
 
-//   useEffect(() => {
-//     const getLanguage = () => {
-//       const lang = initData?.user?.languageCode as string;
-//       langStore.setLang(lang === 'ru' ? 'ru' : 'en')
-//     };
+  useEffect(() => {
+    const getLanguage = () => {
+      const lang = initData?.user?.languageCode as string;
+      langStore.setLang(lang === 'ru' ? 'ru' : 'en')
+    };
 
-//     getLanguage();
-//   }, []);
+    getLanguage();
+  }, []);
 
   
     return (
